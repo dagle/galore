486efeff699b2380f8cadd93ba520dfa771dbda6
Per Odlund <per.odlund@gmail.com>
2022-02-02 18:06:53 +0100

    Make it a bit more lazy, so we don't open the bd before we need it

--- lua/galore/init.lua
+++ lua/galore/init.lua
@@ -1,48 +1,37 @@
--- local v = vim.api
-
--- for k,v in pairs(package.loaded) do
--- 	if string.find(k, "galore.") then
--- 		package.loaded[k] = nil
--- 	end
--- end
-
--- package.loaded["ffi"] = nil
 local config = require("galore.config")
 local saved = require("galore.saved")
--- local compose = require('galore.compose')
--- local jobs = require('galore.jobs')
 local cmp = require("galore.cmp")
 local nu = require("galore.notmuch-util")
 require("galore.gmime").init()
 
-local galore = {
-	-- saved = saved,
-	open = function(opts)
-		vim.fn.sign_define("uncollapsed", { text = "v" })
-		vim.fn.sign_define("collapsed", { text = ">>" })
-		-- idk about this stuff
-		-- if opts ~= nil then
-		-- 	local window = require("notmuch.window." .. opts)
-		-- 	if window ~= nil then
-		-- 		window.create()
-		-- 	else
-		-- 		error("no window module called " .. opts)
-		-- 	end
-		-- end
-		return saved.create("replace")
-		-- create tab etc
-	end,
-	setup = function(opts)
-		nu.gen_config()
-		if opts ~= nil then
-			config.values = vim.tbl_deep_extend("keep", opts, config.values)
-		end
+local galore = {}
+function galore.open(opts)
+	vim.fn.sign_define("uncollapsed", { text = "v" })
+	vim.fn.sign_define("collapsed", { text = ">>" })
+	galore.connect()
+	return saved.create("replace")
+end
 
-		for bind, func in pairs(config.values.key_bindings.global) do
-			vim.api.nvim_set_keymap("n", bind, func, { noremap = true, silent = true })
+function galore.connect(reconnect)
+	if reconnect then
+		galore.connected = false
+	end
+	if not galore.connected then
+		nu.gen_config()
+		if galore.config ~= nil then
+			config.values = vim.tbl_deep_extend("keep", galore.config, config.values)
 		end
-	end,
-	-- we cant to close the db when we are finished
-}
+	end
+	galore.connected = true
+end
+
+
+-- should global functions try to connect?
+function galore.setup(opts)
+	galore.user_config = opts
+	for bind, func in pairs(config.values.key_bindings.global) do
+		vim.api.nvim_set_keymap("n", bind, func, { noremap = true, silent = true })
+	end
+end
 
 return galore
