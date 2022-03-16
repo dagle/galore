fcbccd5717a0cea44f58eef0688b184bf99f4953
Per Odlund <per.odlund@gmail.com>
2022-03-16 07:24:12 +0100

    Add functions to browse the thread in telescope

--- lua/galore/telescope.lua
+++ lua/galore/telescope.lua
@@ -5,7 +5,6 @@ if not ok then
 end
 
 local pickers = require("telescope.pickers")
-local teleconf = require("telescope.config").values
 local previewers = require("telescope.previewers")
 local actions = require("telescope.actions")
 local putils = require("telescope.previewers.utils")
@@ -15,19 +14,16 @@ local action_set = require("telescope.actions.set")
 local compose = require("galore.compose")
 
 local r = require("galore.render")
-local gm = require("galore.gmime")
-local gu = require("galore.gmime-util")
-local guu = require("galore.gmime.util")
-local nm = require("galore.notmuch")
-local conf = require("galore.config")
+local gu = require("galore.gmime.util")
 local message_view = require("galore.message_view")
 local ts_utils = require "telescope.utils"
 local strings = require "plenary.strings"
 local gp = require("galore.gmime.parts")
-local ge = require("galore.gmime.extra")
 local gc = require("galore.gmime.content")
--- local jobs = require("galore.jobs")
+local go = require("galore.gmime.object")
 local ffi = require("ffi")
+local config = require("galore.config")
+local nm = require("galore.notmuch")
 
 local Telescope = {}
 
@@ -42,10 +38,10 @@ local function show_tree(object)
 	elseif gp.is_part(object) then
 		local part = ffi.cast("GMimePart *", object)
 		if gp.part_is_attachment(part) then
-			if guu.part_mime_type(object) == "application/pgp-signature" then
-				return "signature"
-			end
-			return "attachment"
+			-- if gu.part_mime_type(object) == "application/pgp-signature" then
+			-- 	return "signature"
+			-- end
+			return gu.part_mime_type(object)
 		end
 		return "part"
 	elseif gp.is_multipart_encrypted(object) then
@@ -74,38 +70,12 @@ function Telescope.parts_browser(message, selected)
 	state.select = {}
 	state.part = {}
 	gp.message_foreach_dfs(message, browser_fun, state)
-	vim.ui.select(state.select, {}, function (item, idx)
+	vim.ui.select(state.select, {}, function (_, idx)
 		-- apply filters to this?
 		if selected then
 			selected(state.part[idx])
 		end
 	end)
-end
-
-local notmuch_picker = function(opts)
-	-- local db_path = conf.values.db_path
-	local data = {}
-	local search = opts.search or ""
-	-- local db = nm.db_open(db_path, 0)
-	local db = conf.values.db
-	local query = nm.create_query(db, search)
-	for m in nm.query_get_messages(query) do
-		local display = nm.message_get_header(m, "Subject")
-		local filename = nm.message_get_filename(m)
-		-- table.insert(data, {filename, display})
-		table.insert(data, { filename, display })
-	end
-	return query,
-		finders.new_table({
-			results = data,
-			entry_maker = function(entry)
-				return {
-					value = entry[1],
-					display = entry[2],
-					ordinal = entry[2],
-				}
-			end,
-		})
 end
 
 local function type_to_kind(type)
@@ -123,7 +93,7 @@ local function type_to_kind(type)
 end
 
 local function open_path(bufnr, type, path, fun)
-	local message = gm.parse_message(path)
+	local message = gu.parse_message(path)
 	actions.close(bufnr)
 	local mode = type_to_kind(type)
 
@@ -142,7 +112,7 @@ end
 
 local function open_draft(bufnr, type)
 	local entry = action_state.get_selected_entry()
-	local path = entry.value
+	local path = entry.value.filename
 	open_path(bufnr, type, path, load_draft)
 end
 
@@ -155,7 +125,11 @@ local function open_search(bufnr, type)
 	local entry = action_state.get_selected_entry()
 	actions.close(bufnr)
 	local mode = type_to_kind(type)
-	message_view.create(entry.value.filename, mode)
+	local line = {
+		filename=entry.value.filename,
+		tags=entry.value.tags,
+	}
+	message_view:create(line, mode, nil, nil)
 end
 
 -- XXX honor opts
@@ -212,7 +186,7 @@ local function encrypted(buf, winid, message)
 end
 
 local function mime_preview(buf, winid, path)
-	local message = gm.parse_message(path)
+	local message = gu.parse_message(path)
 	r.show_header(message, buf, nil, nil)
 	r.show_message(message, buf, {preview = function (bufid, str)
 		encrypted(bufid, winid, str)
@@ -220,17 +194,22 @@ local function mime_preview(buf, winid, path)
 	putils.highlighter(buf, "mail")
 end
 
-Telescope.notmuch_search = function(opts)
+Telescope.notmuch_search = function(opts, cb)
 	opts = opts or {}
 	local live_notmucher = finders.new_job(function(prompt)
-		if not prompt or prompt == "" then
+		if opts.presearch then
+			if not prompt or prompt == "" then
+				prompt = opts.presearch
+			else
+				prompt = opts.presearch .. " and " .. prompt
+			end
+		end
+		if not prompt or prompt == "" and not opts.search_group then
 			return nil
 		end
-		if opts.presearch then
-			prompt = opts.presearch .. " and " .. prompt
-		end
-
-		return vim.tbl_flatten({ "nm-livesearch", "message", prompt })
+		local group = opts.search_group or "message"
+		local ret =  vim.tbl_flatten({ "nm-livesearch", group, prompt})
+		return ret
 	end, entry_maker(opts), opts.max_results, opts.cwd)
 
 	pickers.new(opts, {
@@ -246,49 +225,92 @@ Telescope.notmuch_search = function(opts)
 			end,
 		}),
 		attach_mappings = function()
-			-- action_set.select:replace(compose_search)
 			action_set.select:replace(open_search)
 			return true
 		end,
 	}):find()
 end
 
-local search_builder = function(opts)
-	opts = opts or {}
-	local q, picker = notmuch_picker(opts)
-	pickers.new(opts, {
-		prompt_title = "Notmuch search",
-		results_title = "Notmuch match",
-		-- finder = notmuch_picker(opts),
-		finder = picker,
-		previewer = previewers.new_buffer_previewer({
-			title = opts.preview_title or "Notmuch preview",
-			keep_last_buf = true,
-			define_preview = opts.preview or function(self, entry, status)
-				local filename = entry.value
-				mime_preview(self.state.bufnr, filename)
-			end,
-		}),
-		attach_mappings = function()
-			action_set.select:replace(open_draft)
-			return true
-		end,
-	}):find()
-	nm.query_destroy(q)
-end
-
 Telescope.load_draft = function(opts)
 	opts = opts or {}
 	local search = opts.search or ""
-	opts.search = search .. "tag:draft"
+	opts.presearch = search .. "tag:draft"
 	opts.prompt_title = "Load draft"
 	opts.results_title = "Drafts"
 	opts.preview_title = "Draft preview"
-	-- opts.preview = function (self, entry, status)
-	-- 	local filename = entry.value
-	-- 	mime_preview(self.state.bufnr, filename)
-	-- end
-	search_builder(opts)
+	opts.attach_mappings = function ()
+			action_set.select:replace(open_draft)
+			return true
+	end
+	Telescope.notmuch_search(opts)
+end
+
+local function make_tag(message)
+	local subject = go.object_get_header(ffi.cast("GMimeObject *", message), "Subject") or ""
+	local from = {vim.fn.bufnr('%'), vim.fn.line('.'), vim.fn.col('.'), 0}
+	return {{tagname=subject, from=from}}
+end
+
+function Telescope.goto_tree(message, opts)
+	opts = opts or {}
+	local realsearch = gp.message_get_message_id(message)
+	opts.presearch = string.format("thread:{mid:%s}", realsearch)
+	opts.prompt_title = "Load References"
+	opts.results_title = "Message"
+	opts.preview_title = "Message Preview"
+	Telescope.notmuch_search(opts)
+end
+
+--- go to all emails before this one
+function Telescope.goto_reference(message, opts)
+	opts = opts or {}
+	local refs = go.object_get_header(ffi.cast("GMimeObject *", message), "References")
+	if refs == nil then
+		vim.notify("No reference")
+		return
+	end
+	local search = opts.search or ""
+	local buf = {}
+	for ref in gc.reference_iter_str(refs) do
+		table.insert(buf, "mid:" .. ref)
+	end
+	opts.presearch = search .. table.concat(buf, " or ")
+	opts.prompt_title = "Load Reference"
+	opts.results_title = "Message"
+	opts.preview_title = "Message Preview"
+	Telescope.notmuch_search(opts)
+end
+
+--- goto all emails after this one
+function Telescope.goto_references(message, opts)
+	local realsearch = gp.message_get_message_id(message)
+	opts.search_group = {"message-after", realsearch}
+	opts.prompt_title = "Load References"
+	opts.results_title = "Message"
+	opts.preview_title = "Message Preview"
+	Telescope.notmuch_search(opts)
+end
+
+--- Move this
+function Telescope.goto_parent(mv)
+	local nu = require("galore.notmuch-util")
+	local ref = go.object_get_header(ffi.cast("GMimeObject *", mv.message), "In-Reply-To")
+	if ref == nil then
+		vim.notify("No reference")
+		return
+	end
+	local realsearch = gc.utils_decode_message_id(ref)
+
+	local query = nm.create_query(config.values.db,  realsearch)
+	local line
+	for nmmessage in nm.query_get_messages(query) do
+		line = nu.get_message(nmmessage)
+		break;
+	end
+
+	local items = make_tag(mv.message)
+	vim.fn.settagstack(vim.fn.win_getid(), {items=items}, 't')
+	message_view:create(line, "replace", nil, nil)
 end
 
 Telescope.attach_file = function(compose, opts)
@@ -309,4 +331,5 @@ Telescope.attach_file = function(compose, opts)
 	end
 	require("telescope").extensions.file_browser.file_browser(opts)
 end
+
 return Telescope
