1ab5d321317ebff6035da39cb5a55a26ce474231
Per Odlund <per.odlund@gmail.com>
2022-03-19 19:43:31 +0100

    A fcc stub

--- lua/galore/hooks.lua
+++ lua/galore/hooks.lua
@@ -1,6 +1,11 @@
 --- A hook takes a message and returns a boolean if success
 
 local gp = require("galore.gmime.parts")
+local go = require("galore.gmime.object")
+local config = require("galore.config")
+local job = require("galore.jobs")
+local Path = require("plenary.path")
+local ffi = require("ffi")
 
 local M = {}
 
@@ -39,6 +44,29 @@ function M.confirm()
 		end
 		return false
 	end)
+end
+
+-- XXX do error handling.
+function M.fcc_nm_insert(message)
+	local obj = ffi.cast("GMimeObject *", message)
+	local fcc_str = go.object_get_header(obj, "Fcc")
+
+	-- Should we try to guard users?
+	-- this shouldn't be an abs path
+	local path = config.values.fccdir .. "/" .. fcc_str
+
+	go.object_set_header(obj, "Fcc", nil)
+	job.insert_mail(message, path, "")
+end
+
+--- write file to a maildir
+--- this should do a lot more checks
+--- create dirs etc if it doesn't exist
+function M.fcc_fs(message)
+	local obj = ffi.cast("GMimeObject *", message)
+	local fcc_str = go.object_get_header(obj, "Fcc")
+
+	go.object_set_header(obj, "Fcc", nil)
 end
 
 function M.preview()
