cb61fdecee77a0d874b60b5ad289753383037458
Per Odlund <per.odlund@gmail.com>
2022-03-22 21:53:42 +0100

    Temp add colors to init

--- lua/galore/init.lua
+++ lua/galore/init.lua
@@ -1,16 +1,19 @@
 local config = require("galore.config")
 local saved = require("galore.saved")
+local runtime = require("galore.runtime")
 local nu = require("galore.notmuch-util")
-require("galore.gmime").init()
+require("galore.gmime.init").init()
 
 local galore = {}
 function galore.open(opts)
+	opts = opts or {}
+	opts.open_mode = opts.open_mode or "replace"
 	require("galore.cmp_nm")
 	require("galore.cmp_vcard")
 	vim.fn.sign_define("uncollapsed", { text = "v" })
 	vim.fn.sign_define("collapsed", { text = ">>" })
 	galore.connect()
-	return saved:create("replace")
+	return saved:create(opts.open_mode)
 end
 
 function galore.connect(reconnect)
@@ -18,15 +21,19 @@ function galore.connect(reconnect)
 		galore.connected = false
 	end
 	if not galore.connected then
-		nu.gen_config()
 		if galore.config ~= nil then
 			config.values = vim.tbl_deep_extend("keep", galore.config, config.values)
 		end
 	end
+	runtime.init()
+	nu.gen_config()
 	galore.connected = true
 end
 
 function galore.setup(opts)
+	-- Move this later
+	vim.cmd("highlight nmVerifyGreen	ctermfg=224 guifg=Green")
+	vim.cmd("highlight nmVerifyRed		ctermfg=224 guifg=Red")
 	galore.user_config = opts
 	for bind, func in pairs(config.values.key_bindings.global) do
 		vim.keymap.set("n", bind, func, { noremap = true, silent = true})
