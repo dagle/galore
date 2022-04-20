9b839c16f5023fab76ade70d30e45c761795fae1
Per Odlund <per.odlund@gmail.com>
2022-04-20 08:32:14 +0200

    Add the new hook system

---
+++ lua/galore/hooks/post.lua
@@ -0,0 +1,36 @@
+--- After a mail have been sent
+local go = require("galore.gmime.object")
+-- Should config really be in here?
+local config = require("galore.config")
+local job = require("galore.jobs")
+local ffi = require("ffi")
+
+local M = {}
+
+-- XXX do error handling.
+function M.fcc_nm_insert(message)
+	local obj = ffi.cast("GMimeObject *", message)
+	local fcc_str = go.object_get_header(obj, "Fcc")
+
+	-- Should we try to guard users?
+	-- this shouldn't be an abs path
+	local path = config.values.fccdir .. "/" .. fcc_str
+
+	go.object_set_header(obj, "Fcc", nil)
+	job.insert_mail(message, path, "")
+end
+
+--- write file to a maildir
+--- this should do a lot more checks
+--- create dirs etc if it doesn't exist
+function M.fcc_fs(message)
+	local obj = ffi.cast("GMimeObject *", message)
+	local fcc_str = go.object_get_header(obj, "Fcc")
+
+	go.object_set_header(obj, "Fcc", nil)
+end
+
+function M.delete_draft(message)
+end
+
+return M
---
+++ lua/galore/hooks/send.lua
@@ -0,0 +1,53 @@
+local gp = require("galore.gmime.parts")
+local M = {}
+
+--- hook design:
+--- They should log when they run
+--- A hook should be able to block and not.
+
+function M.preview_ask(message)
+	-- create preview
+	vim.ui.input({
+		prompt = "Do you want to send email? [Y]es/[N]o"
+	}, function (input)
+		if input then
+			input = input:lower()
+			if input == "yes" or input == "y" then
+				return true
+			end
+		end
+		return false
+	end)
+end
+
+--- @param message any
+--- @return boolean
+function M.has_attachment(message)
+	local find_attachment = function (_, part, state)
+		if gp.is_part(part) and gp.part_is_attachment(part) then
+			state.attachment = true
+		end
+	end
+	local state = {}
+	gp.message_foreach(message, find_attachment, state)
+	return state.attachment
+end
+
+--- Doesn't handle re:
+function M.missed_attachment(message)
+	local sub = gp.message_get_subject(message)
+	if sub:match("[a,A]ttachment") and not M.has_attachment(message) then
+		return false
+	end
+	return true
+end
+
+-- Delay sending for X seconds
+-- Returns a cancel channel and a delay-hook
+-- XXX api for async hooks?
+-- You don't use this directly but to make hooks
+function M.make_delay_hook(seconds)
+end
+
+
+return M
