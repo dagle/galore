5c1da19b670ad071bdcff98f428e2d7e5972465f
Per Odlund <per.odlund@gmail.com>
2022-07-14 18:53:42 +0200

    Clean up and add some startup functionality

--- lua/galore/init.lua
+++ lua/galore/init.lua
@@ -1,46 +1,81 @@
 local config = require("galore.config")
-local runtime = require("galore.runtime")
-local nu = require("galore.notmuch-util")
-require("galore.gmime.init").init()
+local started = false
 
--- vim.fn.sign_define("uncollapsed", { text = "v" })
--- vim.fn.sign_define("collapsed", { text = ">>" })
-
--- use setlocal and move this
--- vim.cmd("hi Folded guibg=None")
--- function _G.custom_fold_text()
--- 	local line = vim.fn.getline(vim.v.foldstart)
--- 	local line_count = vim.v.foldend - vim.v.foldstart + 1
--- 	return line
--- end
--- vim.opt.foldtext = 'v:lua.custom_fold_text()'
 local galore = {}
 function galore.open(opts)
 	opts = opts or {}
 	opts.open_mode = opts.open_mode or "replace"
 	galore.connect()
-	config.values.start(opts)
+	config.values.init(opts)
 end
 
 function galore.connect(reconnect)
 	if reconnect then
 		galore.connected = false
 	end
+	local runtime = require("galore.runtime")
+	local def = require("galore.default")
+	if not started then
+		require("galore.gmime.init").init()
+	end
 	if not galore.connected then
+		def.gen_config()
 		if galore.user_config ~= nil then
 			config.values = vim.tbl_deep_extend("force", config.values, galore.user_config)
 		end
 		runtime.init()
-		nu.gen_config()
 	end
 	galore.connected = true
+	started = true
+end
+
+local function colors()
+	vim.api.nvim_set_hl(0, "GaloreVerifyGreen", {fg="Green"})
+	vim.api.nvim_set_hl(0, "GaloreVerifyRed", {fg="Red"})
+	vim.api.nvim_set_hl(0, "GaloreSeperator", {fg="Red"})
+	vim.api.nvim_set_hl(0, "GaloreAttachment", {fg="Red"})
+	vim.api.nvim_set_hl(0, "GaloreHeader", {fg="Red"})
 end
 
 function galore.setup(opts)
 	-- Move this later
-	vim.cmd("highlight nmVerifyGreen	ctermfg=224 guifg=Green")
-	vim.cmd("highlight nmVerifyRed		ctermfg=224 guifg=Red")
+	colors()
 	galore.user_config = opts
+end
+
+function galore.compose(args)
+	local opts = {}
+	if type(args) == "string" then
+		args = args:gsub("mailto:", "")
+		if string.gmatch(args, "?") then
+			local lp = args:gsub(".*?", "")
+			for k, v in string.gmatch(lp, "([^&=?]+)=([^&=?]+)" ) do
+				opts[k] = v
+			end
+		end
+		opts.mailto = args:gsub("?.*", "")
+	end
+	if not galore.connected then
+		galore.connect()
+	end
+	require('galore.compose'):create("replace", nil, nil, opts)
+end
+
+--- setup things like
+--- galore.desktop
+--- xdg-mime?
+--- autocrypt?
+function galore.configure()
+	vim.fn.system('xdg-mime default galore.desktop "x-scheme-handler/mailto"')
+	-- vim.ui.input({prompt="Setup autoconfig?"}, function(choice)
+	-- 	if not choice then
+	-- 		return
+	-- 	end
+	-- 	choice = choice:lower()
+	-- 	if choice == "yes" or  choice == "y" then
+	-- 		require("galore.autocrypt").init()
+	-- 	end
+	-- end)
 end
 
 return galore
