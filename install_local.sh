2c5966b0d8d1efd10918affddd43111f22cd515a
Per Odlund <per.odlund@gmail.com>
2022-11-01 11:33:20 +0100

    Shellchecked

--- install_local.sh
+++ install_local.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 # If we want to install it globally, you don't want to run this file
 
-BASEDIR=$(realpath $(dirname "$0"))
+NAME=$(dirname "$0")
+BASEDIR=$(realpath "${NAME}")
 
 if [ -d "${BASEDIR}/build" ]; then
-	rm -r ${BASEDIR}/build
+	rm -r "${BASEDIR}"/build
 fi
-meson ${BASEDIR}/build
-meson install -C ${BASEDIR}/build
-sed -i "s|libgalore.so|${BASEDIR}/build/src/libgalore.so|" ${BASEDIR}/build/src/Galore-0.1.gir
-g-ir-compiler ${BASEDIR}/build/src/Galore-0.1.gir --output ${BASEDIR}/build/src/Galore-0.1.typelib
+meson "${BASEDIR}"/build
+meson install -C "${BASEDIR}"/build
+sed -i "s|libgalore.so|${BASEDIR}/build/src/libgalore.so|" "${BASEDIR}"/build/src/Galore-0.1.gir
+g-ir-compiler "${BASEDIR}"/build/src/Galore-0.1.gir --output "${BASEDIR}"/build/src/Galore-0.1.typelib
