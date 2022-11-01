60e4e1e10d9e87a3cafb11c4d9110be9c720870c
Per Odlund <per.odlund@gmail.com>
2022-11-01 10:58:44 +0100

    Make all paths relative

--- install_local.sh
+++ install_local.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # If we want to install it globally, you don't want to run this file
-meson build
-meson install -C build
-sed -i "s|libgalore.so|$(pwd)/build/src/libgalore.so|" /home/dagle/code/galore/build/src/Galore-0.1.gir
-g-ir-compiler build/src/Galore-0.1.gir --output build/src/Galore-0.1.typelib
+
+BASEDIR=$(realpath $(dirname "$0"))
+
+rm -r ${BASEDIR}/build
+meson ${BASEDIR}/build
+meson install -C ${BASEDIR}/build
+sed -i "s|libgalore.so|${BASEDIR}/build/src/libgalore.so|" ${BASEDIR}/build/src/Galore-0.1.gir
+g-ir-compiler ${BASEDIR}/build/src/Galore-0.1.gir --output ${BASEDIR}/build/src/Galore-0.1.typelib
