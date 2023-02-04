8ece98056a7af846283031c86b123ec5015f6404
Per Odlund <per.odlund@gmail.com>
2023-02-04 15:27:19 +0100

    Just compile the code, don't install

--- install_local.sh
+++ install_local.sh
@@ -7,7 +7,7 @@ BASEDIR=$(realpath "${NAME}")
 if [ -d "${BASEDIR}/build" ]; then
 	rm -r "${BASEDIR}"/build
 fi
-meson "${BASEDIR}"/build
-meson install -C "${BASEDIR}"/build
+meson setup "${BASEDIR}"/build
+meson compile -C "${BASEDIR}"/build
 sed -i "s|libgalore.so|${BASEDIR}/build/src/libgalore.so|" "${BASEDIR}"/build/src/Galore-0.1.gir
 g-ir-compiler "${BASEDIR}"/build/src/Galore-0.1.gir --output "${BASEDIR}"/build/src/Galore-0.1.typelib
